55ae081bdc007abadc80bd3edf87500246d73eb1
Amanda Santos <98029661+Asantosvp@users.noreply.github.com>
2023-11-25 11:56:25 -0300

    fim

---
+++ script.js
@@ -0,0 +1,22 @@
+let nome = ""
+let xp = 5000
+
+if (xp <= 1000){
+    console.log("ferro")
+} else if (1001 || 2000){
+    console.log("bronze")
+} else if (2001 || 5000){
+    console.log("prata")
+} else if (6001 || 7000){
+    console.log("ouro")
+} else if (7001 || 8000){
+    console.log("platina")
+} else if (8001 || 9000){
+    console.log("ascendente")
+} else if (9001 || 1000){
+    console.log("imortal")
+} else if (xp >= 10001){
+    console.log("radiante")
+} else {
+    console.log("O herói de nome " + nome + " está no nível " + xp)
+}
